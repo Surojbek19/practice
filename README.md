1723f24107f8cc3ba916ee38c6ee22fa09372ae0
Sam <surojbeksv@gmail.com>
2026-08-18 15:16:07 +0900

    feat: python system variables and special method via dunders

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ git config --global user.name "your_name"
 git config --global user.email
 git config --global user.email "your_email"
 git init
+git remote add origin https://github.com/Example_name/example.git
 ```
--- run.py
+++ run.py
@@ -1,4 +1,18 @@
-a = "Alex"
-print("a:", a)
-message = "Hello world!"
+# a = "Alex"
+# print("a:", a)
+# message = "Hello world!"
+# print(message)
+
+message = "In python, everything is an object, varibles are names that refers to object"
 print(message)
+
+result = type(message)
+print("result:", result)
+
+'''In Python, there are builtin tools:
+(1) TYPES > int, float, str, list, dict
+(2) FUCTIONS > print(), type(), len(), input()
+(3) CONSTANTS > True, False, None
+'''
+
+print(dir(__builtins__))
