4795ca8c914eecc72f5db6debd03ea47728233c5
Sam <surojbeksv@gmail.com>
2026-08-20 17:20:27 +0900

    feat: learn specific error handiling in Python

--- objects.py
+++ objects.py
@@ -17,3 +17,25 @@ print(type(math))
 # OOP 4 consepts > Encapsulation, Abstraction, Inheritance, and Polymorphism
 result1 = math.ceil(97.7)
 print(result1)
+
+
+print("==== Error handlig syste ====")
+
+car_obj = dict(name="Tayota", year=2024, electric=False)
+
+try:
+    print("Passed here!!!")
+    a = car_obj.speed
+    # here error happens so this line and other belove lines dont work
+    result = car_obj["country"]
+    print("result:", result)
+except KeyError as err:
+    # if ther is no error, then this line doesnt work
+    print("This kind of object key doesnt exsit:", err)
+else:
+    # if everything works fine, then this line works
+    print("Execution completed!!!")
+
+finally:
+    # this line works with or without error case
+    print("Final closing logic!!!")
