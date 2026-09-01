4b698eb2d803487a8c9bb7ca554f4319a8f6e968
Sam <surojbeksv@gmail.com>
2026-09-02 01:08:54 +0900

    feat: leanr conditions in Python

--- opercon.py
+++ opercon.py
@@ -36,3 +36,45 @@ print(id(c), id(d))  # this is their references here, and they are different.
 print("c is d:", c is d)
 # Now here "e" and "c" have same references, coz e equals c (e = c)
 print("e is c:", e is c)
+
+
+print("===== Conditions =====")
+# Condistions doesn't check true or false but they check thruthy and falsey, coz they  are different things
+x = 5
+if x > 50:
+    print("Case A")
+elif x > 10:
+    print("Case B")
+else:  # else runs when all other coditions are not satisfied and else is considered none-conditional
+    print("Case C")
+
+print("====== Logical operators ======")
+age = 23
+# person = None
+
+# if age > 13:
+#     person = "adult"
+# else:
+#     person = "child"
+
+# Ternery operator
+
+# This done by using  ternery operator and it makes the logic more simplier.
+person = "adult" if age > 18 else "minor"
+
+print("person:", person)
+
+print("********")
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = True
+
+if not is_student:
+    print("Welcome here, do you wanna be a student?")
+elif is_admin:
+    print("Please go to the teachers' room!")
+elif is_guest or is_parent:  # "or" makes it true even if there is one truthy value and others are falesy. But "and" makes it False even if there is only one falesy
+    print("The waiting room is on the first floor!")
+else:
+    print("In other cases!!!")
